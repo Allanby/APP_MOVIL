02201fc3df7c78b036bc5835f83f13f2289526f8
Allan Barillas <allanbarillas2005@gmail.com>
2025-10-03 21:35:57 -0600

    tr

--- app/src/main/java/com/example/admin_rh/LoginActivity.kt
+++ app/src/main/java/com/example/admin_rh/LoginActivity.kt
@@ -10,8 +10,8 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.adminrh.R
 
 class LoginActivity : AppCompatActivity() {
-    private val user = ""
-    private val pass = ""
+    private val user = "sexo"
+    private val pass = "sexo"
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
