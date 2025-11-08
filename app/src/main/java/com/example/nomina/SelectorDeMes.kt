d91153095453b7f5e9dcfb25e283c3a052443879
WendyBlandon05 <juju49019@gmail.com>
2025-11-07 20:31:06 -0600

    Icono de calendario

--- app/src/main/java/com/example/nomina/SelectorDeMes.kt
+++ app/src/main/java/com/example/nomina/SelectorDeMes.kt
@@ -2,6 +2,7 @@ package com.example.adminrh.nomina.components
 
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -12,6 +13,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
@@ -21,6 +24,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.adminrh.R
+
 @Composable
 fun SelectorDeMes(
     mesActual: String,
@@ -29,7 +33,6 @@ fun SelectorDeMes(
     // fuente
     val timesBold = FontFamily(Font(R.font.timesbold))
 
-
     var activo by remember { mutableStateOf(false) }
     val fondoAnimado by animateColorAsState(
         targetValue = if (activo) Color(0xFF004AAD) else Color(0xFF0059D6),
@@ -37,25 +40,36 @@ fun SelectorDeMes(
         label = "animColor"
     )
 
-    // cntenedor principal
+    // contenedor principal
     Surface(
         modifier = modifier
             .fillMaxWidth()
             .height(70.dp)
             .shadow(10.dp, shape = RoundedCornerShape(16.dp))
             .background(fondoAnimado, shape = RoundedCornerShape(16.dp))
-            .padding(horizontal = 16.dp, vertical = 8.dp)
-            .then(Modifier),
+            .padding(horizontal = 16.dp, vertical = 8.dp),
         color = Color.Transparent,
         shape = RoundedCornerShape(16.dp),
         onClick = { activo = !activo }
     ) {
-        Box(
+        Row(
             modifier = Modifier
                 .fillMaxSize()
                 .background(fondoAnimado, shape = RoundedCornerShape(16.dp)),
-            contentAlignment = Alignment.Center
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
         ) {
+
+            Image(
+                painter = painterResource(id = R.drawable.calendar_month),
+                contentDescription = "icono de calendario",
+                modifier = Modifier
+                    .size(35.dp)
+                    .padding(end = 8.dp),
+                contentScale = ContentScale.Fit
+            )
+
+
             Text(
                 text = mesActual,
                 style = TextStyle(
